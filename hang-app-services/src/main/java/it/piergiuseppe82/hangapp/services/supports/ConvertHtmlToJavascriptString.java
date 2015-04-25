862ac2b7f861dd93b45ab96e63afe71bdba9ebb6
piergiuseppe82 <piergiuseppe82@gmail.com>
2015-04-25 20:07:55 +0200

    Main method removed - Spring boot require only one 'main' method on scanned package's tree

--- hang-app-services/src/main/java/it/piergiuseppe82/hangapp/services/supports/ConvertHtmlToJavascriptString.java
+++ hang-app-services/src/main/java/it/piergiuseppe82/hangapp/services/supports/ConvertHtmlToJavascriptString.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
 
 public class ConvertHtmlToJavascriptString {
 	
-	public static void main(String[] args) throws FileNotFoundException {
-		Scanner scanner = new Scanner(new File("test.html")); 
+	public static void convert(String filePath) throws FileNotFoundException {
+		Scanner scanner = new Scanner(new File(filePath)); 
 		System.out.println("var item = \"\";");
 		while(scanner.hasNextLine()){			
 			System.out.println("item = item + \""+scanner.nextLine().replaceAll("\"","'")+"\";");
